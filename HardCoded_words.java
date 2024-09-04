1ca0214f341b51acad776194e67698f23d535717
Ajay-Jangir <Ajayjangir023@gmail.com>
2024-09-04 16:19:46 +0530

    Add files via upload

---
+++ HardCoded_words.java
@@ -0,0 +1,8 @@
+
+public class HardCoded_words {
+    public String Append_ENTER_STRING = "Enter a string: ";
+    public String Append_ENTER_SUBSTRING = "Enter the new String to append: ";
+    public String Append_UPDATED_STRING = "Updated String is: ";
+    public String Append_PROMPT_YES_NO = "If you want to append more then press Y else N: ";
+
+}
